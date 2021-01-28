274e2dbe6c6f10c3e8784192234f10f9f463609f
yqq <1037123792@qq.com>
2021-01-29 00:40:21 +0800

    code

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
-
-
-    
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <h2>{{msg}}</h2>
+
   </div>
 </template>
 
